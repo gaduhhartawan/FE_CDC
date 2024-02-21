c9716db847af2b604903fb280888cd1430b2931c
Frinaldi Muhammad Syauqi <frinaldi.syauqi@gmail.com>
2024-02-21 17:09:30 +0700

    add header, add font, add ico, add grid card

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,24 @@
 import React from "react";
 import Card from "../components/Card";
+import Header from "../components/Header";
 
 const Home = () => {
   return (
-    <div className="p-5 h-screen flex flex-col items-center justify-between gap-5">
-      <h1 className="font-semibold text-2xl">Home Page</h1>
-      <Card />
+    <div>
+      <Header />
+      <div className="p-5 h-screen flex flex-col items-center justify-between gap-5">
+      {/* <h1 className="font-semibold text-2xl">Home Page</h1> */}
+        <div class="grid grid-rows-2 grid-flow-col gap-8">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
       <span></span>
     </div>
+  </div>
   );
 };
 
