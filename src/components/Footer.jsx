109b72f346039f1f91760935d8e63418ce1f7029
Frinaldi Muhammad Syauqi <frinaldi.syauqi@gmail.com>
2024-02-22 23:23:02 +0700

    about pages

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="p-5 flex justify-center items-center">
-      All Rights Reserved • Copyright careetpath by tryfix 2024 in Bandung
+    <footer className="p-5 flex justify-center items-center font-plusjakarta mt-20 text-textabt">
+      All Rights Reserved • Copyright careerpath by tryfix 2024 in Bandung
     </footer>
   );
 };
