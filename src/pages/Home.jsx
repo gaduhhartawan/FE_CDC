3dd4b478e16acb00f3b61078e66d2ec7fc6742a8
Gaduh Hartawan <gaduhhartaone@gmail.com>
2024-02-23 23:15:17 +0700

    feat: delete unused file

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,6 @@
 import React from "react";
 import Card from "../components/Card";
-import Header from "../components/Header";
 import SearchBox from "../components/SearchBox";
-import FilterCard from "../components/FilterCard";
-import Footer from "../components/Footer";
 import { useGetJobs } from "../hooks/api/jobs/useJobsQuery";
 import { Link } from "react-router-dom";
 
