e1884132425bf6afea93918e02b7f4f058d18ec1
Gaduh Hartawan <gaduhhartaone@gmail.com>
2024-02-26 08:59:04 +0700

    feat: update pages

--- src/pages/PostJobView.jsx
+++ src/pages/PostJobView.jsx
@@ -5,8 +5,7 @@ import { PlusIcon } from "@heroicons/react/24/outline";
 import { useGetMyJob } from "../hooks/api/jobs/useJobsQuery";
 import Loading from "../components/Loading";
 import NoJobs from "../components/NoJobs";
-import Modal from "./Modal";  
-
+import Modal from "./Modal";
 
 const PostJobView = () => {
   const [showModal, setShowModal] = React.useState(false);
@@ -15,41 +14,49 @@ const PostJobView = () => {
   const userId = currentUser._id;
   const { data, isLoading } = useGetMyJob(userId);
   if (isLoading) {
-    return <Loading isLoading={isLoading}/>;
+    return <Loading isLoading={isLoading} />;
   }
   return (
     <>
       <div className="w-full h-fit flex flex-col items-center">
-          <div className="flex flex-col lg:flex-row lg:items-end mb-6 lg:w-1/2 w-full lg:justify-between items-center">
-            <div className="flex flex-col md:gap-y-4 items-center lg:items-start mb-5 lg:mb-0 w-1/2">
-              <span className="font-bold text-2xl">
-                My Jobs Post
-              </span>
-              <span className="lg:block hidden text-base">
-                A portal that allows you to manage all your job postings in one
-                place.
-              </span>
-            </div>
-            <Modal showModal={showModal} setShowModal={setShowModal} data={currentJob}/>
-            <Link 
-              to="/postjob"
-              className="flex flex-row items-center bg-bluu text-white rounded-lg gap-x-2 w-40 h-10 justify-center">
-              <PlusIcon className="h-6 w-6"/>
-              <span className="md:text-base text-s text-center">
-                Add new Post
-              </span>
-            </Link>
+        <div className="flex flex-col lg:flex-row lg:items-end mb-6 lg:w-1/2 w-full lg:justify-between items-center">
+          <div className="flex flex-col md:gap-y-4 items-center lg:items-start mb-5 lg:mb-0 w-1/2">
+            <span className="font-bold text-2xl">My Jobs Post</span>
+            <span className="lg:block hidden text-base">
+              A portal that allows you to manage all your job postings in one
+              place.
+            </span>
           </div>
-          <div className="flex flex-col items-center lg:w-1/2 w-full">
-            <div className="flex flex-col gap-5 w-full">
-              <NoJobs data={data?.length} />
-              {data.length ? data?.map((job) => (
-                <>
-                  <CardEdit key={job._id} data={job} setShowModal={setShowModal} setCurrentJob={setCurrentJob}/>
-                </>
-              )): null}
-            </div>
+          <Modal
+            showModal={showModal}
+            setShowModal={setShowModal}
+            data={currentJob}
+          />
+          <Link
+            to="/postjob"
+            className="flex flex-row items-center bg-bluu text-white rounded-lg gap-x-2 w-40 h-10 justify-center"
+          >
+            <PlusIcon className="h-6 w-6" />
+            <span className="md:text-base text-s text-center">
+              Add new Post
+            </span>
+          </Link>
+        </div>
+        <div className="flex flex-col items-center lg:w-1/2 w-full">
+          <div className="flex flex-col gap-5 w-full">
+            <NoJobs data={data?.length} />
+            {data.length
+              ? data?.map((job) => (
+                  <CardEdit
+                    key={job._id}
+                    data={job}
+                    setShowModal={setShowModal}
+                    setCurrentJob={setCurrentJob}
+                  />
+                ))
+              : null}
           </div>
+        </div>
       </div>
     </>
   );
