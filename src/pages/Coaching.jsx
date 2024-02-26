3632ebee6c8ed085865387465e671bf979094d03
gaduhhartawan <123380383+gaduhhartawan@users.noreply.github.com>
2024-02-26 10:10:27 +0700

    Update Coaching.jsx

--- src/pages/Coaching.jsx
+++ src/pages/Coaching.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Cookies from "js-cookie";
 import { useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
 
 const Coaching = () => {
-  const token = Cookies.get("token");
+  const user = JSON.parse(localStorage.getItem("currentUser"));
   const navigate = useNavigate();
 
   const handleNavigate = () => {
-    if (!token) {
+    if (!user) {
       Swal.fire({
         icon: "warning",
         title: "Mohon login terlebih dahulu",
@@ -23,6 +22,7 @@ const Coaching = () => {
       navigate("/coaching/chat");
     }
   };
+  
   return (
     <div className="flex lg:flex-row flex-col-reverse justify-between items-center px-10 gap-y-5 h-[73vh]">
       <div className="flex flex-col gap-7 lg:w-1/2">
